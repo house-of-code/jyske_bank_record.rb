5453e4076b0bf916a376d7a7bdccfaf41f87d19f
Thomas B Homburg <thomas@homburg.dk>
2015-09-21 10:13:40 +0200

    Write bank record files correctly

--- lib/jyske_bank_record.rb
+++ lib/jyske_bank_record.rb
@@ -1,20 +1,49 @@
-# Hello
 module JyskeBankRecord
   DATE_FORMAT = "%Y%m%d"
+
   module Type
     PAYMENT_START = 'IB000000000000'
     PAYMENT_END = 'IB999999999999'
+    DOMESTIC_RECORD = 'IB030202000005'
   end
+
+  module AccountType
+    FINANCIAL = '1'
+    BANK = '2'
+  end
+
+  module Currency
+    DKK = 'DKK'
+  end
+
+  module PaymentType
+    CHEQUE = '1'
+    TRANSFER = '2'
+  end
+
+  module NoticeType
+    ATTACHED = '0'
+    SEPARATE = '1'
+    CHEQUE = '2'
+  end
+
   module Record
     NEWLINE = "\r\n"
 
-    def self.write_records(stream, records)
+    def self.stream_records(records)
       return if records.empty?
+
+      stream = StringIO.new
+
+      # Set stream output to cp1252
+      stream.set_encoding(Encoding::CP1252)
 
       records.each do |r|
         r.write(stream)
         stream << NEWLINE
       end
+
+      stream
     end
 
     def self.write_fields(stream, fields)
@@ -66,5 +95,82 @@ module JyskeBankRecord
         Record.write_fields(stream, fields)
       end
     end
+
+    Recipient = Struct.new(:registration_number, :account_number, :name, :address, :address2, :zip_code, :city) do
+      def encode(encoding)
+        each_pair do |key, value|
+          if value.respond_to?(:encode)
+            self[key] = value.encode(encoding)
+          end
+        end
+      end
+    end
+
+    PaymentRecord = Struct.new(:processing_date, :amount, :sender_account_number, :recipient, :entry_text, :reference, :notice) do
+      def fields
+        start_fields = [
+            Type::DOMESTIC_RECORD,
+            '0001',
+            processing_date.strftime(DATE_FORMAT),
+            amount.abs.to_s.rjust(13, '0') + (amount && '+' || '-'),
+            Currency::DKK,
+            AccountType::BANK,
+            sender_account_number.rjust(15, '0'), # sender account number
+            PaymentType::TRANSFER,
+            recipient.registration_number,
+            recipient.account_number,
+            NoticeType::ATTACHED,
+            entry_text.ljust(35),
+            recipient.name.ljust(32),
+            recipient.address.ljust(32),
+            recipient.address2.ljust(32),
+            recipient.zip_code,
+            recipient.city.ljust(32),
+            reference.ljust(35),
+        ]
+        end_fields = [
+            ' ',
+            ' ' * 215
+        ]
+
+        start_fields + notice_chunks.map { |c| c.ljust(35) } + end_fields
+      end
+
+      # Chunk notice over
+      def notice_chunks
+        # Split to lines
+        notice_lines = notice.strip().gsub("\r", '').split("\n")
+        notice_lines.flat_map do |line|
+          encoding = Encoding::CP1252
+          line = line.encode(encoding)
+
+          # Chunk to 35 chars
+          arr = []
+          until line.empty?
+            arr << line.slice!(0..34)
+          end
+          arr
+        end
+
+        if notice_lines.length < 9
+          notice_lines = notice_lines + (Array.new(9-notice_lines.length) { '' })
+        end
+        notice_lines
+      end
+
+      def write(stream)
+        # Encode fields
+        each_pair do |key, value|
+          if value.respond_to?(:encode)
+            self[key] = value.encode(Encoding::CP1252)
+          end
+        end
+
+        recipient.encode(Encoding::CP1252)
+
+        Record.write_fields(stream, fields)
+      end
+    end
+
   end
 end
--- spec/jyske_bank_record_spec.rb
+++ spec/jyske_bank_record_spec.rb
@@ -9,7 +9,7 @@ module JyskeBankRecord
       end
 
       it 'should have records' do
-        record = PaymentStartRecord.new(Date.today)
+        expect { PaymentStartRecord.new(Date.today) }.not_to raise_error
       end
 
       it 'should write records to a stream' do
@@ -21,22 +21,142 @@ module JyskeBankRecord
         expect(stream.string).to match(/"20150813"/)
       end
 
-      it 'should write a bank record file' do
-        stream = StringIO.new 
+      it 'a stream with encoding should output an encoded string' do
+        stream = StringIO.new
 
+        stream.set_encoding(Encoding::CP1252)
+
+        stream << 'tæst'
+
+        str = stream.string
+
+        expect(str.length).to eq("t\xe6st".length)
+        expect("t\xe6st").to eq("t\xe6st")
+        expect(str.bytes).to eq("t\xe6st".bytes)
+        expect(str).to eq("tæst".encode(Encoding::CP1252))
+      end
+
+      it 'should write a bank record file' do
         date = Date.new(2015, 9, 17)
         start_record = PaymentStartRecord.new date
         end_record = PaymentEndRecord.new date, []
 
         records = [start_record, end_record]
 
-        Record.write_records(stream, records)
+        stream = Record.stream_records(records)
 
         File.open(File.join(__dir__, "files", "empty")) do |f|
           left = stream.string
           right = f.read
           expect(left.length).to eq(right.length)
           expect(left).to eq(right)
+        end
+      end
+
+      it 'should write a bank record file with transactions' do
+        date = Date.new(2015, 9, 17)
+        start_record = PaymentStartRecord.new date
+
+        transactions = [
+          PaymentRecord.new(
+              Date.new(2015, 9, 25),
+              747,
+              '123456789',
+              Recipient.new(
+                '6666',
+                '9876543210',
+                'nævn 1',
+                'modtager-adresse-1',
+                'modtager-adresse-2',
+                '5000',
+                'Odense',
+              ),
+              'posteringstekst 1',
+              'eget bilags-nr',
+              (1..9).map{ |n| "advisering #{n}" }.join("\n")
+          ),
+          PaymentRecord.new(
+              Date.new(2015, 9, 25),
+              566,
+              '1234567890',
+              Recipient.new(
+                  '7777',
+                  '0123456789',
+                  'navn 2',
+                  'modtager-adresse-1-2',
+                  'modtager-adresse-2-2',
+                  '6000',
+                  'Kolding',
+              ),
+              'posteringstekst 2',
+              'eget bilags-nr 2',
+              ''
+          ),
+        ]
+
+        end_record = PaymentEndRecord.new date, transactions
+
+        records = [start_record] + transactions + [end_record]
+
+        stream = Record.stream_records(records)
+
+        File.open(File.join(__dir__, "files", "2_records"), encoding: Encoding::CP1252) do |f|
+          left = stream.string
+          right = f.read
+          expect(left.length).to eq(right.length)
+          expect(left).to eq(right)
+        end
+      end
+
+      describe PaymentRecord do
+        it 'should 9 produce notice chunks' do
+          record = PaymentRecord.new(
+              Date.new(2015, 9, 25),
+              747,
+              '123456789',
+              Recipient.new(
+                  '6666',
+                  '9876543210',
+                  'nævn 1',
+                  'modtager-adresse-1',
+                  'modtager-adresse-2',
+                  '5000',
+                  'Odense',
+              ),
+              'posteringstekst 1',
+              'reference',
+              'notice'
+          )
+
+          chunks = record.notice_chunks
+
+          expect(chunks.length).to eq(9)
+          expect(chunks).to eq(['notice'] + (Array.new(8) { '' }))
+        end
+
+        it 'should write to a 896 byte record' do
+          record = PaymentRecord.new(
+              Date.new(2015, 9, 25),
+              747,
+              '123456789',
+              Recipient.new(
+                  '6666',
+                  '9876543210',
+                  'nævn 1',
+                  'modtager-adresse-1',
+                  'modtager-adresse-2',
+                  '5000',
+                  'Odense',
+              ),
+              'posteringstekst 1',
+              'reference',
+              'notice'
+          )
+
+          stream = Record.stream_records([record])
+
+          output = stream.string
+          expect(output.length).to eq(896)
         end
       end
     end
